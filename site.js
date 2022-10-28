87402e1298399e65113c7d8718c383cbde3a4ecc
konstantintzt <49292114+konstantintzt@users.noreply.github.com>
2022-10-28 00:53:37 +0000

    Initial commit

---
+++ site.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+const app = express()
+
+app.use(express.static("public"))
+
+app.listen(process.env.PORT, function() {
+    console.log("website running on port " + process.env.PORT)
+})
