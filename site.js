eef2d1af645c46a7d6ce373dc28b3e1473f9c668
konstantintzt <49292114+konstantintzt@users.noreply.github.com>
2022-11-22 04:36:34 +0000

    Finished HTTPS migration

--- site.js
+++ site.js
@@ -1,12 +1,22 @@
 const express = require("express")
 const dotenv = require("dotenv")
+const favicon = require("serve-favicon")
+const path = require("path")
+const https = require("https")
+const fs = require("fs")
 
 dotenv.config()
 
 const app = express()
 
 app.use(express.static("public"))
+// app.use(favicon(path.join(__dirname, "public", "favicon.ico")))
 
-app.listen(process.env.PORT, function() {
+https.createServer({
+    key: fs.readFileSync("privkey.pem"),
+    cert: fs.readFileSync("cert.pem"),
+    ca: fs.readFileSync("chain.pem")
+}, app)
+.listen(3000, function() {
     console.log("website running on port " + process.env.PORT)
 })
